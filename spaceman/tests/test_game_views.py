82e2b593eefd6578248a59b5ef482c94428a7fe5
dakoatatyson <dakoatatyson@gmail.com>
2020-01-27 02:02:04 -0600

    added get_solution unit tests
    Addressed #4

--- spaceman/tests/test_game_views.py
+++ spaceman/tests/test_game_views.py
@@ -20,14 +20,18 @@ class GameApiViewTests( TestCase ):
             'id': None
         }
 
+        self.expected_solution_word = {
+            "solution": "batman"
+        }
+
         self.mock_game = Game(
-                word = "TESTWORD",
-                guesses_allowed = self.expected_game_data['guesses_allowed'],
-                guesses_taken = self.expected_game_data['guesses_taken'],
-                letters_guessed = self.expected_game_data['letters_guessed'],
-                guessed_word_state = self.expected_game_data['guessed_word_state'],
-                is_game_over = self.expected_game_data['is_game_over']
-            )
+            word = "batman",
+            guesses_allowed = self.expected_game_data['guesses_allowed'],
+            guesses_taken = self.expected_game_data['guesses_taken'],
+            letters_guessed = self.expected_game_data['letters_guessed'],
+            guessed_word_state = self.expected_game_data['guessed_word_state'],
+            is_game_over = self.expected_game_data['is_game_over']
+        )
 
         self.request_factory = APIRequestFactory()
         self.mock_get_request = self.request_factory.get('dummy')
@@ -67,9 +71,20 @@ class GameApiViewTests( TestCase ):
             mock_get.assert_called_with( pk=25 )
             self.assertEquals( response.status_code, 400 )
 
-
     ### GET solution view
-    # TODO: Add tests for Getting a game's solution
     # HINT: remember the `setUp` fixture that is in this test class, 
     #   it constructs things that might be useful
+    def test_get_solution_should_respond_with_404_when_id_not_found( self ):
+        with patch.object( Game.objects, 'get' ) as mock_get:
+            mock_get.side_effect = Game.DoesNotExist
 
+            response = game_solution( self.mock_get_request, 25 )
+            self.assertEqual( response.status_code, 404 )
+
+
+    def test_get_solution_should_respond_with_solution_word_json( self ):
+        with patch.object( Game.objects, 'get' ) as mock_get:
+            mock_get.return_value = self.mock_game
+
+            response = game_solution( self.mock_get_request, 25 )
+            self.assertEqual( response.data, { "solution": "batman"} )
